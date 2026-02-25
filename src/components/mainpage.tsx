230f08a8efd83481d7d77fd28c9f507491e53755
9948286998 <developer-email@example.com>
2026-02-25 21:32:33 +0530

    nothing

--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -60,7 +60,7 @@ export default function Header() {
                             textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
                         }}
                     >
-                        Shahin's Mehndi  -------------
+                        Shahin's Mehndi updateded
                     </h1>
 
                 </div>
